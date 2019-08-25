aa5a7fe78fd3ef922d2c482c6a1aa0dfae6afd37
ludde <ludde_bodin@hotmail.com>
2019-08-25 20:52:33 +0200

    num

--- start.py
+++ start.py
@@ -7,13 +7,12 @@ api_key = '5dJG4cUIU2mS7o82slCTsMRjO'
 URL = 'https://www.amazon.com/GoPro-HERO7-Black-Waterproof-Streaming-Stabilization/dp/B07GDGZCCH/ref=sr_1_3?keywords=gopro&qid=1566756913&s=gateway&sr=8-3'
 
 headers = {'User-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36'}
-
-
+phone_number = '+46xxxxxxxx'
 
 def send_sms(title, price):
     client = messagebird.Client(api_key)
     try:
-        msg = client.message_create('+46766526406', '+46766526406', f'Hello Ludvig, the {title} is ready to be bought! the price is {price}')
+        msg = client.message_create(phone_number, phone_number, f'Hello Ludvig, the {title} is ready to be bought! the price is {price}')
         print(msg.__dict__)
         print('sms sent')
     except messagebird.client.ErrorException as e:
